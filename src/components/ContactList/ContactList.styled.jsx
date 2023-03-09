ae983bac29258f0887bf0844ef2748ca188e33b7
Haber Viacheslav <gaberslavik@gmail.com>
2023-03-09 11:05:23 +0200

    added first commit

--- src/components/ContactList/ContactList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ContactListWrp = styled.ul`
-  font-size: 20px;
-  display: flex;
-  gap: 8px;
-  flex-direction: column;
-  align-items: flex-start;
-`;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from 'redux/store';
 import { App } from 'components/App';
 import './index.css';
-import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
